38c683294da274bb6e2633df2dcc70cff68581ba
MayankTagra <mayanktagra1@gmail.com>
2019-03-28 17:08:08 +0530

    Another Commit

--- SalesForceTestProject/src/CustomerSuccess/FilterBy.java
+++ SalesForceTestProject/src/CustomerSuccess/FilterBy.java
@@ -99,5 +99,53 @@ public class FilterBy extends BaseClass {
 			driver.close();
 			
 		}
+		@Test()
+		public void ClearAll() throws InterruptedException
+		{
+			WebDriver driver=Login();
+			driver.get("https://www.salesforce.com/in/customer-success-stories/");
+			JavascriptExecutor js=(JavascriptExecutor)driver;
+			js.executeScript("window.scrollBy(0,2000)");
+			driver.findElement(By.xpath("//h4[@data-target='#collapse_T']")).click();
+			Thread.sleep(10000);
+			driver.findElement(By.xpath("//div[@id='collapse_T']//li[2]//a")).click();
+			Thread.sleep(10000);
+			driver.findElement(By.xpath("//h4[@data-target='#collapse_T']")).click();
+			Thread.sleep(3000);
+			driver.findElement(By.xpath("//h4[@data-target='#collapse_P']")).click();
+			Thread.sleep(10000);
+			driver.findElement(By.xpath("//div[@id='collapse_P']//li[1]//a")).click();
+			Thread.sleep(10000);
+			driver.findElement(By.xpath("//h4[@data-target='#collapse_P']")).click();
+			Thread.sleep(3400);
+			driver.findElement(By.xpath("//h4[@data-target='#collapse_I']")).click();
+			Thread.sleep(10000);
+			driver.findElement(By.xpath("//div[@id='collapse_I']//li[1]//a")).click();
+			Thread.sleep(10000);
+			driver.findElement(By.xpath("//h4[@data-target='#collapse_I']")).click();
+			Thread.sleep(3400);
+			
+			driver.findElement(By.xpath("//h4[@data-target='#collapse_BS']")).click();
+			Thread.sleep(4000);
+			driver.findElement(By.xpath("//div[@id='collapse_BS']//li[1]//a")).click();
+			Thread.sleep(10000);
+			driver.findElement(By.xpath("//h4[@data-target='#collapse_BS']")).click();
+			Thread.sleep(3400);
+			driver.findElement(By.xpath("//h4[@data-target='#collapse_BT']")).click();
+			Thread.sleep(4000);
+			driver.findElement(By.xpath("//div[@id='collapse_BT']//li[1]//a")).click();
+			Thread.sleep(10000);
+			driver.findElement(By.xpath("//h4[@data-target='#collapse_BT']")).click();
+			Thread.sleep(3000);
+			js.executeScript("window.scrollBy(0,-800)");
+			Thread.sleep(5000);
+			
+			driver.findElement(By.xpath(" //div[@class='filters-search-container']//a[@href='/in/customer-success-stories/']//span")).click();
+			Thread.sleep(5000);
+			
+			driver.close();
+			
+			
+		}
 		
 }
