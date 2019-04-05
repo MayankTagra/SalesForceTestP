d20323c5e818004c423103f788f24f22fc37e259
MayankTagra <mayanktagra1@gmail.com>
2019-04-05 18:30:24 +0530

    SSA

---
+++ SalesForceTestProject/TestRun1.bat
@@ -0,0 +1,5 @@
+set projectPath="c:\Users\mtagra\git\repository4\SalesForceTestProject";
+cd\
+cd %projectPath%;
+set classPath=%projectPath%\bin;%projectPath%\lib\*;
+java org.testng.TestNG %projectPath%\lib\testng2.xml;
--- SalesForceTestProject/src/SupportAndServices/Ideas.java
+++ SalesForceTestProject/src/SupportAndServices/Ideas.java
@@ -30,10 +30,63 @@ public class Ideas extends BaseClass{
 		
 	}
 	@Test()
-	public void StatusFilterDropdown()
+	public void StatusFilterDropdown() throws InterruptedException
 	{
 		WebDriver driver=Login();
-		driver.get("");
+		driver.get("https://success.salesforce.com/ideaSearch?sort=2&filter=Delivered+in+Winter+17");
+		
+		Thread.sleep(3200);
+		int count=driver.findElement(By.xpath("//div[@class='overlay-menu overlay-menu-select'][2]//ul[@class='ul-reset']"))
+				.findElements(By.tagName("li")).size();
+		System.out.println(count);
+		Thread.sleep(4000);
+		
+
+		for(int i=1;i<23;i++)
+		{
+			driver.findElement(By.xpath("//button[@id='idea_status_filter_opener']")).click();
+			Thread.sleep(5400);
+			driver.findElement(By.xpath("//div[@class='overlay-menu overlay-menu-select'][2]//li["+i+"]")).click();
+			Thread.sleep(3400);
+
+		}
+		
+		driver.close();
+	}
+	@Test()
+	public void SelectAllOptionsOnLeft() throws InterruptedException
+	{
+		WebDriver driver=Login();
+		driver.get("https://success.salesforce.com/ideaSearch");
+		Thread.sleep(3200);
+		int count=driver.findElement(By.xpath("//ul[@class='ul-reset ul-nav2']"))
+				.findElements(By.tagName("li")).size();
+		System.out.println(" count "+count);
+		
+		for(int i=1;i<=count;i++)
+		{
+			driver.findElement(By.xpath("//ul[@class='ul-reset ul-nav2']//li["+i+"]//a")).click();
+			int innerCount=driver.findElement(By.xpath("//ul[@class='ul-reset ul-nav2']"
+					+ "//li[@class='selected']//ul[@class='ul-reset ul-nav2']"))
+					.findElements(By.tagName("li")).size();
+			System.out.println("InnerCount "+innerCount);
+
+			for(int k=1;k<=innerCount;k++)
+			{
+				
+					driver.findElement(By.xpath("//ul[@class='ul-reset ul-nav2'"
+							+ "]//li["+i+"]"
+							+ "//ul[@class='ul-reset ul-nav2']//li["+k+"]")).click();
+						
+						Thread.sleep(5400);
+						
+						//clicked 
+					
+			
+			}
+			
+		}
+		
 	}
 
 }
