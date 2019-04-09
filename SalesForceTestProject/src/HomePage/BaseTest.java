edd4988beb1ab084045b76a51ce5cbf80c3df15c
MayankTagra <mayanktagra1@gmail.com>
2019-04-09 15:46:40 +0530

    Waiting game

--- SalesForceTestProject/src/HomePage/BaseTest.java
+++ SalesForceTestProject/src/HomePage/BaseTest.java
@@ -3,8 +3,8 @@ package HomePage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
 import com.aventstack.extentreports.ExtentReports;
@@ -14,56 +14,54 @@ import com.aventstack.extentreports.markuputils.ExtentColor;
 import com.aventstack.extentreports.markuputils.MarkupHelper;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
-
-
 public class BaseTest {
 	public static ExtentHtmlReporter htmlReporter;
 	public static ExtentReports extent;
 	public static ExtentTest test;
-	public WebDriver driver;
+
+	
+	WebDriver driver;
+
+	
+	
 	
 	public WebDriver Login()
 	{
 		System.setProperty("webdriver.chrome.driver", "c:\\chromeDriver\\chromedriver.exe");
-			driver=new ChromeDriver();
-			return driver;
-			
+		driver=new ChromeDriver();
+		return driver;
 	}
-	public String url()
-	{
-		return "https://www.salesforce.com/in/cro/index-smb/?ir=1";
-	}
-	
 	@BeforeSuite()
-	public void Setup()
+	public static void Setup()
 	{
-		htmlReporter=new ExtentHtmlReporter(System.getProperty("user.dir")+"/test-output/SalesForceTestReport.html");
+		htmlReporter=new ExtentHtmlReporter(System.getProperty("user.dir")+"/test-output/SalesReport.html");
 		extent=new ExtentReports();
 		extent.attachReporter(htmlReporter);
 		
 	}
-	
-	@BeforeMethod()
-	public void getData(ITestResult result)
-	{
+	@AfterMethod()
+	public static void getResult(ITestResult result)
+	{	
 		if(result.getStatus()==ITestResult.FAILURE)
 		{
-			test.log(Status.FAIL, MarkupHelper.createLabel(result.getName()+"The Test case failed ue to"
-					+ " the following reason", ExtentColor.RED));
+			test.log(Status.FAIL, MarkupHelper.createLabel(result.getName()+"Test case Failed ue to following reason", ExtentColor.RED));
 			test.fail(result.getThrowable());
 		}
 		else if(result.getStatus()==ITestResult.SUCCESS)
 		{
-			test.log(Status.PASS,MarkupHelper.createLabel(result.getName()+"Test case passed", ExtentColor.GREEN));
+			test.log(Status.PASS, MarkupHelper.createLabel(result.getName()+"Test Case Passed Successfully", ExtentColor.GREEN));
+			
 		}
 		else
 		{
-			test.skip("The given tezt case has been skipped");
+			test.skip("Test case has been skipped");
 		}
+		
 	}
 	@AfterSuite()
-	public void tearDown()
+	public static void tearDown()
 	{
 		extent.flush();
 	}
-}
+
+	}
